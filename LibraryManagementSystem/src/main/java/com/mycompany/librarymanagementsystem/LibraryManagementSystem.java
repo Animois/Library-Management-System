c85c90f9ea66e1bf1980e19630e9f475bc7f2609
Shrayas Chakma <125141065+Animois@users.noreply.github.com>
2024-01-19 13:54:02 +0600

    Add files via upload

---
+++ LibraryManagementSystem/src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.librarymanagementsystem;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Shrayas Chakma
+ */
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+//        new Login().setVisible(true);
+        JFrame login = new Login();
+        login.setVisible(true);;
+    }
+}
---
+++ LibraryManagementSystem/src/main/java/com/mycompany/librarymanagementsystem/Student.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.librarymanagementsystem;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Shrayas Chakma
+ */
+class Student {
+
+    void setBook(returnBook.Book book) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
+
+        
+        
+        
+        
